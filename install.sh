a8e9580bbe1c8ac572ae99b4f6e1e677a512526b
Peter Morris <podmorris@mac.com>
2015-11-21 01:09:56 +0000

    Added -P flag to pwd to avoid recursive symlinking

--- install.sh
+++ install.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 rm ~/.vim
-VIM_FOLDER=$(cd $(dirname $0) && pwd)
+VIM_FOLDER=$(cd $(dirname $0) && pwd -P)
 ln -s $VIM_FOLDER ~/.vim
 git --git-dir=$VIM_FOLDER/.git --work-tree=$VIM_FOLDER submodule update --init --recursive
 $VIM_FOLDER/bundle/YouCompleteMe/install.py
